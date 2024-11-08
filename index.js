9df8610d57568f14413e186ed0e1b85e3ddc59b5
monubawane2861 <monubawane2861>
2024-11-08 23:06:17 +0530

    first commit

---
+++ index.js
@@ -0,0 +1,48 @@
+
+// let themeBtn = document.getElementById("themeBtn");
+
+//  function onThemeBtn(){
+
+//   let button = document.createElement("button");
+
+//   button.classList.add("theme_btn")
+
+//   themeBtn.appendChild(button);
+
+
+
+//   let icon = document.createElement("i")
+
+//   icon.classList.add("fa-regular fa-moon");
+
+//   button.appendChild(icon)
+
+// }
+
+
+let toggA = document.getElementById("toggA");
+
+function toggleNme(){
+
+
+  toggA.classList.add("togshow");
+
+}
+
+
+let toggB = document.getElementById("toggB");
+
+function onKnowmore(){
+
+
+  toggB.classList.add("togshoww");
+
+
+}
+
+
+ 
+
+
+<i class="fa-solid fa-delete-left"></i>
+
